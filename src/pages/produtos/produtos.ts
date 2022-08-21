51f0e5b5b8ee4627fcc83b95117a01cdfbd4b55f
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-08-21 17:10:38 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,7 +3,7 @@ import { Dev_Config } from './../../config/dev.config';
 import { Categoria } from './../../models/categoria';
 
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { CategoriaService } from '../../services/models/categoria.service';
 
 
@@ -18,23 +18,28 @@ export class ProdutosPage {
 	items:Categoria[]=[]
 
   constructor(public navCtrl: NavController, public navParams: NavParams ,
-		public catService:CategoriaService, public alertCtrl:AlertController) {
+		public catService:CategoriaService, public alertCtrl:AlertController,
+		public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('cat_id')
+		let loader = this.presentLoading()
 		this.catService.findById(categoria_id)
 		.subscribe(response=>{
 
 		this.items = response['content']
-		console.log(this.items)
+
 	this.items.map((resposta)=> {
 	this.items =resposta.produtos
 	this.loadImageUrls()
-	console.log(resposta.produtos)
-	})
+
+	}
+	)
+	loader.dismiss()
 },
 error=>{
+	loader.dismiss()
 })
 }
 
@@ -55,6 +60,15 @@ loadImageUrls(){
 
 showDetails(produto_id:string){
 	this.navCtrl.push('ProdutoDetailPage',{prod_id:produto_id})
+}
+
+presentLoading() {
+	const loader = this.loadingCtrl.create({
+		content: "Aguarde um momento...",
+
+	});
+	loader.present();
+	return loader
 }
 
 		}
